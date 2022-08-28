d6ac028c897b8487172686ccade3b2f6e45fca10
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-28 23:38:35 +1000

    timestamp parsing

---
+++ misc/test_misc.py
@@ -0,0 +1,13 @@
+import misc
+
+def test_padded_min_sec():
+    assert misc.parse_timestamp('04-57') == '04:57'
+
+def test_2_digit_min_single_sec():
+    assert misc.parse_timestamp('59-8') == '59:8'
+
+def test_over_sec():
+    assert misc.parse_timestamp('5-60') is None
+
+def test_over_hour():
+    assert misc.parse_timestamp('61-3') is None
