83aaf76bfc31c7a3b3448e43c86b15cfc311ecd8
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-28 14:31:15 +1000

    different approach needed for relative imports

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 `mve` must exist in the parent level folder  
 
 `run-pytest` will copy the most recent version of `mve` to the current folder
++ first runs `get-mve-files`
--- test_01_treater.py
+++ test_01_treater.py
@@ -1,6 +1,11 @@
-def a():
-    return 10
+from subprocess import CalledProcessError
+import pytest
 
-def test_a():
-    assert a() == 10
+import mve.src.treater as treater
 
+def test_edit_good():
+    try:
+        output_name = 'test.mp4'
+        treater.edit_video('videos/03.mp4', output_name, '2', '4')
+    except CalledProcessError as c:
+        assert False, str(c)
