cc3a6c956cbbb9cbf9beec92ad2e274476a33270
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-04 00:53:28 +1000

    experimenting with escape codes

--- colour/experiment.py
+++ colour/experiment.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
+
 r = '\033[1;0m' # reset
 
 colours = {
-    'orange attempt': '\033[2;103m'
+    'orange attempt': '\033[2;103m', # bright yellow background
+    'yellow text': '\033[1;103m', # bright yellow background pickle colour text
+    'after white': '\033[1;38m'
 }
+
+def attempt(key):
+    print(f'attempt {key}: {colours[key]}text{r} end')
+
+for c in colours:
+    attempt(c)
