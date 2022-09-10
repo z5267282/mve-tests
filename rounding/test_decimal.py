1fd20655e6d9ff5c38cb17fb20755dc48f338594
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-10 23:00:27 +1000

    decimal desting

---
+++ rounding/test_decimal.py
@@ -0,0 +1,5 @@
+import decimal
+
+def test_round_up():
+    dec = decimal.Decimal('1.57')
+    assert dec.quantize(decimal.Decimal('1.'), rounding=decimal.ROUND_HALF_EVEN).compare(decimal.Decimal('2')) == 1
