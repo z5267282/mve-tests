0b64dd03fb821727f0a8c665f8fc180d0e23f101
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-11 21:34:05 +1000

    adding information to editing tests

--- editing/test_edit.py
+++ editing/test_edit.py
@@ -11,33 +11,23 @@ def generate_out_name(name):
     return f'{OUT}/{name}.mp4'
 
 SRC = '../videos/03.mp4'
-def good_helper(name, start, end=None):
-    try:
-        output_name = generate_out_name(name)
-        edit.edit_video(SRC, output_name, start, end)
-    except CalledProcessError as c:
-        assert False, str(c)
+def helper(name, start, end=None):
+    output_name = generate_out_name(name)
+    edit.edit_video(SRC, output_name, start, end)
 
 BAD = generate_out_name('bad')
 def bad_helper(start, end):
     with pytest.raises(CalledProcessError):
         edit.edit_video(SRC, BAD, start, end)
 
-# GOOD TESTS 
-
 def test_good_seconds_start_end():
-    good_helper('start end', '2', '4')
+    helper('start end', '2', '4')
 
 def test_good_timestamp():
-    good_helper('2 timestamps', '00:22', '00:24')
+    helper('2 timestamps', '00:22', '00:24')
 
 def test_good_positive_second():
-    good_helper('only start', '71')
+    helper('only start', '67')
 
 def test_good_negative_end():
-    good_helper('negative end', '-2')
-
-# BAD TESTS
-
-def test_bad_over_bounds():
-    bad_helper('1:22', '1:24')
+    helper('negative end', '-2')
