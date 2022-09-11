cd90c7cd33e8e8b304d04670224bb1a211ee8ae1
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-11 23:57:47 +1000

    decimal library doomed

--- rounding/test_decimal.py
+++ rounding/test_decimal.py
@@ -2,4 +2,4 @@ import decimal
 
 def test_round_up():
     dec = decimal.Decimal('1.57')
-    assert dec.quantize(decimal.Decimal('1.'), rounding=decimal.ROUND_HALF_EVEN).compare(decimal.Decimal('2')) == 1
+    assert dec.quantize(decimal.Decimal('2.'), rounding=decimal.ROUND_HALF_EVEN).compare(decimal.Decimal('2')) == 0
