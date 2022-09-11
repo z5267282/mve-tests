ba652319b74d45e6d155165daf8906f68deb5bd2
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-11 21:23:14 +1000

    adding extra files needed for video editing

---
+++ editing/config.py
@@ -0,0 +1,17 @@
+# folder paths
+SOURCE      = ['D:', '\\', 'TF2 BIG', 'Team Fortress 2', '3']
+RENAMES     = ['D:', '\\', 'Videos', 'MC Renames']
+DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'Minecraft']
+
+# multi threading and processing
+NUM_THREADS   = 4
+NUM_PROCESSES = 4 
+
+# MoviePy
+USE_MOVIEPY = True
+
+# testing
+TESTING = False
+
+# colours
+BOLD = False 
