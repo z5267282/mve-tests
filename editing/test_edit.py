0330363977e2b169bc5be6077df65fe9b17b46bc
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-11 21:51:02 +1000

    moving around test order and deleting outputs

--- editing/test_edit.py
+++ editing/test_edit.py
@@ -3,30 +3,27 @@ from subprocess import CalledProcessError
 import edit
 import pytest
 
-# HELPERS
+# TESTS
 
-OUT = 'outputs'
-def generate_out_name(name):
-    return f'{OUT}/{name}.mp4'
+# def test_good_seconds_start_end():
+#     helper('start end', '4', '10')
 
-SRC = '../videos/03.mp4'
-def helper(name, start, end=None):
-    output_name = generate_out_name(name)
-    edit.edit_video(SRC, output_name, start, end)
-
-BAD = generate_out_name('bad')
-def bad_helper(start, end):
-    with pytest.raises(CalledProcessError):
-        edit.edit_video(SRC, BAD, start, end)
-
-def test_good_seconds_start_end():
-    helper('start end', '4', '10')
-
-def test_good_timestamp():
-    helper('2 timestamps', '00:25', '00:30')
+# def test_good_timestamp():
+#     helper('2 timestamps', '00:25', '00:30')
 
 def test_good_positive_second():
     helper('only start', '60')
 
-def test_good_negative_end():
-    helper('negative end', '-4')
+# def test_good_negative_end():
+#     helper('negative end', '-4')
+
+# HELPERS
+
+def generate_out_name(name):
+    OUT = 'outputs'
+    return f'{OUT}/{name}.mp4'
+
+def helper(name, start, end=None):
+    SRC = '../videos/03.mp4'
+    output_name = generate_out_name(name)
+    edit.edit_video(SRC, output_name, start, end)
