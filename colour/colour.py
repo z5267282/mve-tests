05090cca7a4b62838e1651a08043587d3468821b
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-11 21:06:38 +1000

    fixing test

--- colour/colour.py
+++ colour/colour.py
@@ -1,9 +1,13 @@
-BLACK  = '\033[1;30m'
-RED    = '\033[1;31m'
-GREEN  = '\033[1;32m'
-YELLOW = '\033[1;33m'
-BLUE   = '\033[1;34m'
-PURPLE = '\033[1;35m'
-CYAN   = '\033[1;36m'
-WHITE  = '\033[1;37m'
-RESET  = '\033[1;0m'
+import config as cfg
+
+
+# cannot make a static variable so manually insert int value
+BLACK  = f'\033[{int(cfg.BOLD)};30m'
+RED    = f'\033[{int(cfg.BOLD)};31m'
+GREEN  = f'\033[{int(cfg.BOLD)};32m'
+YELLOW = f'\033[{int(cfg.BOLD)};33m'
+BLUE   = f'\033[{int(cfg.BOLD)};34m'
+PURPLE = f'\033[{int(cfg.BOLD)};35m'
+CYAN   = f'\033[{int(cfg.BOLD)};36m'
+WHITE  = f'\033[{int(cfg.BOLD)};37m'
+RESET  = f'\033[{int(cfg.BOLD)};0m'
