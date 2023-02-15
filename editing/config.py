3d7a4ecaa2aafc1d10284c77a0e79ba30fe31b1e
Sunny Chen <z5267282@student.unsw.edu.au>
2023-02-15 13:03:55 +1100

    starting on swapper testing

--- editing/config.py
+++ editing/config.py
@@ -1,7 +1,7 @@
 # folder paths
 SOURCE      = ['D:', '\\', 'Videos', 'Minecraft']
 RENAMES     = ['D:', '\\', 'Videos', 'Summer Minecraft Events']
-DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'Summer Minecraft']
+DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'Minecraft 23']
 
 # multi threading and processing
 NUM_THREADS   = 4
