7671cdc8acded1492512c4246052cfab657890ee
Sunny Chen <z5267282@student.unsw.edu.au>
2023-02-16 23:15:39 +1100

    accidentally deleted files

---
+++ swapper-swap/config.py
@@ -0,0 +1,17 @@
+# folder paths
+SOURCE      = ['..', 'vids', 'tokyo']
+RENAMES     = [".."]
+DESTINATION = ["..", "outputs"]
+
+# multi threading and processing
+NUM_THREADS   = 4
+NUM_PROCESSES = 4
+
+# MoviePy
+USE_MOVIEPY = False
+
+# testing
+TESTING = True
+
+# colours
+BOLD = False
