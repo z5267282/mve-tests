867e3b627fc06945a3d28c313b1d56a8673dddf6
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-02 00:48:08 +1000

    fixing to only generate first 97 colours

--- generate-test-video/generate.py
+++ generate-test-video/generate.py
@@ -19,7 +19,8 @@ fourcc = cv2.VideoWriter.fourcc(*'mp4v')
 video_writer = cv2.VideoWriter('generated.mp4', fourcc, FPS, (1920, 1080))
 
 # Iterate over the RGB values and create frames
-for rgb in colours:
+for i, rgb in enumerate(colours[:LEN]):
+    print(f'colour {i:2d} loaded')
     # the tuple is (height, width, num. colour values: 3 in "RGB")
     frame = np.zeros((1080, 1920, 3), np.uint8)
     frame[:, :, :] = rgb
@@ -30,3 +31,5 @@ for rgb in colours:
 
 # Release the video writer
 video_writer.release()
+
+print(f'successfully created video generated.mp4')
