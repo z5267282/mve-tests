693df31ea7560892e9036a8418c2b7cb50333ef4
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-11 13:38:16 +1100

    fixing make-flags

--- make-flags/test_folders.py
+++ make-flags/test_folders.py
@@ -1,13 +1,13 @@
 import json
 import pytest
 
-import constants.options as options
+import mve.src.constants.options as options
+
 
 def test_folders():
     with open('configs/fishing/config.json', 'r') as f:
         data = json.load(f)
 
-    assert data[options.SOURCE] == ['..', '..', 'videos']
-    assert data[options.RENAMES] == [".."]
-    assert data[options.DESTINATION] == ["..", "..", "outputs"]
-
+    assert data[options.SOURCE] == ['..', 'videos']
+    assert data[options.RENAMES] == ['..']
+    assert data[options.DESTINATION] == ['outputs']
