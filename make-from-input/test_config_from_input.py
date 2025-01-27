61b16438812cfa9b635be5eb865d1f917afe7b21
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-28 00:19:33 +1100

    make from input works

--- make-from-input/test_config_from_input.py
+++ make-from-input/test_config_from_input.py
@@ -7,7 +7,7 @@ from mve.src.config import Stateful
 
 def test_config_paths_correctly_loaded():
     with open(os.path.join(
-            os.environ['MVE_CONFIGS'], os.environ['CONFIG'], Stateful.CONFIG)
+            os.environ['MVE_CONFIGS'], 'input-make', Stateful.CONFIG)
     ) as f:
         conf = json.load(f)
 
