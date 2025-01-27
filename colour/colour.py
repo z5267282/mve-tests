ae5a9191e4a68011006042baa198e09b2a3193a8
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-28 00:00:30 +1100

    removing stale config

--- colour/colour.py
+++ colour/colour.py
@@ -1,13 +1,12 @@
-import config as cfg
-
+BOLD = True
 
 # cannot make a static variable so manually insert int value
-BLACK  = f'\033[{int(cfg.BOLD)};30m'
-RED    = f'\033[{int(cfg.BOLD)};31m'
-GREEN  = f'\033[{int(cfg.BOLD)};32m'
-YELLOW = f'\033[{int(cfg.BOLD)};33m'
-BLUE   = f'\033[{int(cfg.BOLD)};34m'
-PURPLE = f'\033[{int(cfg.BOLD)};35m'
-CYAN   = f'\033[{int(cfg.BOLD)};36m'
-WHITE  = f'\033[{int(cfg.BOLD)};37m'
-RESET  = f'\033[{int(cfg.BOLD)};0m'
+BLACK = f'\033[{int(BOLD)};30m'
+RED = f'\033[{int(BOLD)};31m'
+GREEN = f'\033[{int(BOLD)};32m'
+YELLOW = f'\033[{int(BOLD)};33m'
+BLUE = f'\033[{int(BOLD)};34m'
+PURPLE = f'\033[{int(BOLD)};35m'
+CYAN = f'\033[{int(BOLD)};36m'
+WHITE = f'\033[{int(BOLD)};37m'
+RESET = f'\033[{int(BOLD)};0m'
